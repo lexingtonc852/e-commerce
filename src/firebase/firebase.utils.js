2eaae82c0d1e06f49aa460ae4f4c30ef19edd766
lexingtonc852 <lexingtonc852@gmail.com>
2020-07-15 18:40:03 +0800

    Updated firebase config

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+  apiKey: "AIzaSyDmqGPOJ3XIfIqF5o2ZbRHJqvCtowJzRfA",
+  authDomain: "e-commerce-e5dfc.firebaseapp.com",
+  databaseURL: "https://e-commerce-e5dfc.firebaseio.com",
+  projectId: "e-commerce-e5dfc",
+  storageBucket: "e-commerce-e5dfc.appspot.com",
+  messagingSenderId: "289911938910",
+  appId: "1:289911938910:web:2efbb8c2411c14fc87f9cf",
+  measurementId: "G-H61MCRK9HR"
 };
 
 firebase.initializeApp(config);
